476f21d29941a5373a6d3524f9243861530d63b3
Taha <bit21it07@gmail.com>
2024-07-06 18:24:28 +0530

    deleted profile and setting page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:apptest/pages/home_page.dart';
-import 'package:apptest/pages/settings_page.dart';
-import 'package:apptest/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,10 +17,6 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
-      routes: {
-        '/settings': (context) => const SettingsPage(),
-        '/profile': (context) => const ProfilePage(),
-      },
     );
   }
 }
